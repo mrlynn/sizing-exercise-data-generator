50aae029015e513abd5cdc48dec4b4891091ba66
Michael Lynn <merlynn@gmail.com>
2017-12-04 06:15:27 -0500

    Add explain version of query

---
+++ query-explain.js
@@ -0,0 +1,57 @@
+db.surveys.aggregate(
+    
+        // Pipeline
+        [
+            // Stage 1
+            {
+                $unwind: {
+                    path : "$survey"
+                }
+            },
+    
+            // Stage 2
+            {
+                $match: {
+                    "recv_dt": {
+                                    "$gte": ISODate("2014-01-01T16:45:23.509Z"),
+                                    "$lte": ISODate("2016-03-21T15:45:23.509Z")
+                                },
+                    "survey_status": 3
+                }
+            },
+    
+            // Stage 3
+            {
+                $group: {
+                    "_id": {
+                       "category": "$survey.question_category"
+                    },
+                    "average": {
+                        "$avg": "$survey.response"
+                    }
+                }
+            },
+    
+            // Stage 4
+            {
+                $project: {
+                    "category": "$_id.category",
+                    "average": 1,
+                    "_id": 0
+                }
+            },
+        ],
+    
+        // Options
+        {
+            cursor: {
+                batchSize: 50
+            },
+    
+            explain: true
+        }
+    
+        // Created with Studio 3T, the GUI for MongoDB - https://studio3t.com/
+    
+    );
+    
